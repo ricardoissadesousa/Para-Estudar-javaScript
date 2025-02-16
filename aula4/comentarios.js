6ecb5daf671ba6b20a508aefc44f4d6fb2aa425b
ricardoissadeousa <ricardoissadesousa@gmail.com>
2025-02-16 18:25:23 -0300

    aula comentarios

---
+++ aula4/comentarios.js
@@ -0,0 +1,21 @@
+// E escrever um comentario 
+console.log("Ola mundo!") // Aqui tem outro comentario
+console.log("Linha 1");
+ 
+
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+// Vai ser ignorado
+
+
+/**
+ * Um titulo
+ * Lorem ipsum
+ * 
+ */
+
+/*
+Um bloco de codigo
+Linha de comentario 
+*/
