11a7f938e914975c7671a220c5faf2b733b93dd7
ricardoissadeousa <ricardoissadesousa@gmail.com>
2025-02-16 15:31:29 -0300

    primeiro commit

---
+++ aula3/ex/ex.js
@@ -0,0 +1,2 @@
+console.log('Meu nome e "Ricardo".Estou aprendendo javaScript às ' , 10 ,  ' da manhâ ');
+// pode usar uma virgula para diferenciar ou concatenando com + da certo tbm 
---
+++ aula3/index.js
@@ -0,0 +1,7 @@
+console.log("'Ricardo'"); // String = texto 
+console.log('"Issa "de Sousa') // Number = Numero 
+
+console.log(`'Ricardo' "Issa"`)
+
+console.log(12345,15.85)
+
